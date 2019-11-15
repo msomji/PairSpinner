52b2bbc6b39759e934bc6504f5fe7abccda4f0c0
Maisam Somji <maisam.somji@gmail.com>
2019-11-15 00:11:32 -0500

    delete all components and clean up bulma

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,21 +3,18 @@ import { Route } from 'react-router'
 import { BrowserRouter } from 'react-router-dom'
 
 import './App.scss'
-import TeamsComponent from './Components/TeamsComponent/TeamsComponent'
-import Profile from './Components/Profile/Profile'
-import NavBar from './Components/NavBar/NavBar'
-import Footer from './Components/Footer/Footer'
+
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="app">
-        <NavBar />
+        {/* <NavBar />
         <main className="has-background-light">
           <Route exact path="/" component={TeamsComponent} />
           <Route exact path="/profile/:teamName" component={Profile} />
         </main>
-        <Footer />
+        <Footer /> */}
       </div>
     </BrowserRouter>
   )
